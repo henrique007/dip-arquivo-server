e510ebd05e767b61b4578ddb5422f5541a32812d
henrique007 <dayvsonn.ti@outlook.com>
2022-05-31 10:10:59 -0300

    PORT config

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,6 @@ app.get("/signin/callback", (request, response) => {
   return response.json(code)
 })
 
-app.listen(4000, () => console.log('Server is running on port 4000'));
+app.listen(process.env.PORT || 4000, () => console.log('Server is running on port 4000'));
 
 
